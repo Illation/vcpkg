2ec8898fe5bf083015655e68195157e6bdcba940
Tim Mayberry <mojofunk@gmail.com>
2018-04-29 11:11:19 +1000

    [atkmm] Add a CMake based build for atkmm
    The existing MSBuild based build system was using a gendef utility to
    define/generate the function exports. The gendef utility would
    occasionally (sometimes consistently) abort.
    
    Moving to a CMake build means the WINDOWS_EXPORT_ALL_SYMBOLS
    functionality can be used instead of this internal/in-tree gendef
    utility.
    
    The install paths follow upstream/unix conventions for locations which
    means it can be used (probably with some additions and testing) on other
    platforms and hopefully be pushed upstream at some point, once things
    like pkg-config support have been added.

--- ports/atkmm/portfile.cmake
+++ ports/atkmm/portfile.cmake
@@ -13,57 +13,49 @@ vcpkg_download_distfile(ARCHIVE
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
-vcpkg_apply_patches(
-    SOURCE_PATH ${SOURCE_PATH}
-    PATCHES ${CMAKE_CURRENT_LIST_DIR}/fix_properties.patch ${CMAKE_CURRENT_LIST_DIR}/fix_charset.patch
-)
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/msvc_recommended_pragmas.h DESTINATION ${SOURCE_PATH}/MSVC_Net2013)
-
-set(VS_PLATFORM ${VCPKG_TARGET_ARCHITECTURE})
-if(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
-    set(VS_PLATFORM "Win32")
-endif(${VCPKG_TARGET_ARCHITECTURE} STREQUAL x86)
-vcpkg_build_msbuild(
-    PROJECT_PATH ${SOURCE_PATH}/MSVC_Net2013/atkmm.sln
-    TARGET atkmm
-    PLATFORM ${VS_PLATFORM}
-    USE_VCPKG_INTEGRATION
-)
-
-# Handle headers
-file(COPY ${SOURCE_PATH}/MSVC_Net2013/atkmm/atkmmconfig.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(COPY ${SOURCE_PATH}/atk/atkmm.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
 file(
-    COPY
-    ${SOURCE_PATH}/atk/atkmm
+    COPY ${CMAKE_CURRENT_LIST_DIR}/atkmmconfig.h.cmakein
+    DESTINATION ${SOURCE_PATH}/atk
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
+)
+
+vcpkg_install_cmake()
+
+vcpkg_copy_pdbs()
+
+set(ATKMM_TARGET atkmm-1.6)
+
+# Move Headers
+file(
+    RENAME ${CURRENT_PACKAGES_DIR}/include/${ATKMM_TARGET}/atkmm/
+    ${CURRENT_PACKAGES_DIR}/include/atkmm
+)
+
+file(
+    COPY ${CURRENT_PACKAGES_DIR}/include/${ATKMM_TARGET}/
     DESTINATION ${CURRENT_PACKAGES_DIR}/include
     FILES_MATCHING PATTERN *.h
 )
 
-# Handle libraries
 file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/atkmm.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/bin
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Release/${VS_PLATFORM}/bin/atkmm.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/lib
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/atkmm.dll
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
-)
-file(
-    COPY
-    ${SOURCE_PATH}/MSVC_Net2013/Debug/${VS_PLATFORM}/bin/atkmm.lib
-    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+    RENAME ${CURRENT_PACKAGES_DIR}/lib/${ATKMM_TARGET}/include/atkmmconfig.h
+    ${CURRENT_PACKAGES_DIR}/include/atkmmconfig.h
 )
 
-vcpkg_copy_pdbs()
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/${ATKMM_TARGET})
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/${ATKMM_TARGET})
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright and readme
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/atkmm RENAME copyright)
